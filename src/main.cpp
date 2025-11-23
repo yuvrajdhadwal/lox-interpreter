9a3a72132bf67c5140abfd20f0a4633d9c3bc021
Yuvraj Dhadwal <ydhadwal3@gatech.edu>
2025-11-23 13:52:51 -0800

    ensured comments are now line only

--- src/main.cpp
+++ src/main.cpp
@@ -59,87 +59,97 @@ int parse_characters(std::string_view file_contents)
 {
     int exit_code {0};
     int line_number {1};
+    bool is_comment {false};
+
     for (int i {0}; i < file_contents.length(); ++i)
     {
         std::string rel_op_parse{};
-        switch (file_contents[i])
+        if (is_comment && file_contents[i] == '\n')
         {
-        case '(':
-            std::cout << "LEFT_PAREN ( null\n";
-            break;
-        case ')':
-            std::cout << "RIGHT_PAREN ) null\n";
-            break;
-        case '{':
-            std::cout << "LEFT_BRACE { null\n";
-            break;
-        case '}':
-            std::cout << "RIGHT_BRACE } null\n";
-            break;
-        case ',':
-            std::cout << "COMMA , null\n";
-            break;
-        case '.':
-            std::cout << "DOT . null\n";
-            break;
-        case '-':
-            std::cout << "MINUS - null\n";
-            break;
-        case '+':
-            std::cout << "PLUS + null\n";
-            break;
-        case ';':
-            std::cout << "SEMICOLON ; null\n";
-            break;
-        case '/':
-            if (i + 1 < file_contents.length() && file_contents[i + 1] == '/')
-            {
-                return exit_code;
-            }
-
-            std::cout << "SLASH / null\n";
-            break;
-        case '*':
-            std::cout << "STAR * null\n";
-            break;
-        case '=':
-        case '!':
-        case '<':
-        case '>':
-            if (file_contents[i] == '=')
-            {
-                rel_op_parse = "EQUAL";
-            } else if (file_contents[i] == '!')
-            {
-                rel_op_parse = "BANG";
-            } else if (file_contents[i] == '<')
-            {
-                rel_op_parse = "LESS";
-            } else
-            {
-                rel_op_parse = "GREATER";
-            }
-
-            if (i + 1 < file_contents.length() && file_contents[i + 1] == '=')
-            {
-                std::cout << rel_op_parse << "_EQUAL " << file_contents[i] << "= null\n";
-                i += 1;
-            } else {
-                std::cout << rel_op_parse << ' ' << file_contents[i] << " null\n";
-            }
-            break;
-        case ' ':
-        case '\t':
-            break;
-        case '\n':
+            is_comment = false;
             ++line_number;
-            break;
-        default:
-            std::cerr << "[line " << line_number
-                << "] Error: Unexpected character: " << file_contents[i] << '\n';
-            exit_code = 65;
-            break;
-        }   
+        } else if (!is_comment)
+        {
+            switch (file_contents[i])
+            {
+            case '(':
+                std::cout << "LEFT_PAREN ( null\n";
+                break;
+            case ')':
+                std::cout << "RIGHT_PAREN ) null\n";
+                break;
+            case '{':
+                std::cout << "LEFT_BRACE { null\n";
+                break;
+            case '}':
+                std::cout << "RIGHT_BRACE } null\n";
+                break;
+            case ',':
+                std::cout << "COMMA , null\n";
+                break;
+            case '.':
+                std::cout << "DOT . null\n";
+                break;
+            case '-':
+                std::cout << "MINUS - null\n";
+                break;
+            case '+':
+                std::cout << "PLUS + null\n";
+                break;
+            case ';':
+                std::cout << "SEMICOLON ; null\n";
+                break;
+            case '/':
+                if (i + 1 < file_contents.length() && file_contents[i + 1] == '/')
+                {
+                    is_comment = true;
+                    break;
+                }
+
+                std::cout << "SLASH / null\n";
+                break;
+            case '*':
+                std::cout << "STAR * null\n";
+                break;
+            case '=':
+            case '!':
+            case '<':
+            case '>':
+                if (file_contents[i] == '=')
+                {
+                    rel_op_parse = "EQUAL";
+                } else if (file_contents[i] == '!')
+                {
+                    rel_op_parse = "BANG";
+                } else if (file_contents[i] == '<')
+                {
+                    rel_op_parse = "LESS";
+                } else
+                {
+                    rel_op_parse = "GREATER";
+                }
+
+                if (i + 1 < file_contents.length() && file_contents[i + 1] == '=')
+                {
+                    std::cout << rel_op_parse << "_EQUAL " << file_contents[i] << "= null\n";
+                    i += 1;
+                } else {
+                    std::cout << rel_op_parse << ' ' << file_contents[i] << " null\n";
+                }
+                break;
+            case ' ':
+            case '\t':
+                break;
+            case '\n':
+                ++line_number;
+                break;
+            default:
+                std::cerr << "[line " << line_number
+                    << "] Error: Unexpected character: " << file_contents[i] << '\n';
+                exit_code = 65;
+                break;
+            }
+        }
     }
 
     return exit_code;
