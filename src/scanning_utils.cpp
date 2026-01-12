e3da71328b9a36ef52a106a55b1ec97486f0ccad
Yuvraj Dhadwal <ydhadwal3@gatech.edu>
2026-01-12 18:40:00 -0500

    debugged properly

--- src/scanning_utils.cpp
+++ src/scanning_utils.cpp
@@ -8,13 +8,13 @@ void parse_characters(std::string_view file_contents, int& exit_code)
     for (size_t i {0}; i < file_contents.length(); ++i)
     {
         std::string rel_op_parse{};
-        if (is_comment && file_contents[i] == '\n')
+        if (is_comment && file_contents.at(i) == '\n')
         {
             is_comment = false;
             ++line_number;
         } else if (!is_comment)
         {
-            switch (file_contents[i])
+            switch (file_contents.at(i))
             {
             case '(':
                 std::cout << "LEFT_PAREN ( null\n";
@@ -71,14 +71,14 @@ void parse_characters(std::string_view file_contents, int& exit_code)
                 parse_string_literal(file_contents, i, line_number, exit_code);
                 break;
             default:
-                if (isdigit(file_contents[i]))
+                if (isdigit(file_contents.at(i)))
                 {
                     parse_number(file_contents, i);
                     break;
                 } else
                 {
                     std::cerr << "[line " << line_number
-                        << "] Error: Unexpected character: " << file_contents[i] << '\n';
+                        << "] Error: Unexpected character: " << file_contents.at(i) << '\n';
                     exit_code = 65;
                     break;
                 }
@@ -90,13 +90,13 @@ void parse_characters(std::string_view file_contents, int& exit_code)
 void parse_relational_op(std::string_view file_contents, size_t& i)
 {
     std::string rel_op_parse{};
-    if (file_contents[i] == '=')
+    if (file_contents.at(i) == '=')
     {
         rel_op_parse = "EQUAL";
-    } else if (file_contents[i] == '!')
+    } else if (file_contents.at(i) == '!')
     {
         rel_op_parse = "BANG";
-    } else if (file_contents[i] == '<')
+    } else if (file_contents.at(i) == '<')
     {
         rel_op_parse = "LESS";
     } else
@@ -106,10 +106,10 @@ void parse_relational_op(std::string_view file_contents, size_t& i)
 
     if (i + 1 < file_contents.length() && file_contents[i + 1] == '=')
     {
-        std::cout << rel_op_parse << "_EQUAL " << file_contents[i] << "= null\n";
+        std::cout << rel_op_parse << "_EQUAL " << file_contents.at(i) << "= null\n";
         i += 1;
     } else {
-        std::cout << rel_op_parse << ' ' << file_contents[i] << " null\n";
+        std::cout << rel_op_parse << ' ' << file_contents.at(i) << " null\n";
     }
 }
 
@@ -118,9 +118,9 @@ void parse_string_literal(std::string_view file_contents, size_t& i, int& line_n
     size_t string_start {i};
     ++i;
 
-    while (i < file_contents.length() && file_contents[i] != '"')
+    while (i < file_contents.length() && file_contents.at(i) != '"')
     {
-        if (file_contents[i] == '\n')
+        if (file_contents.at(i) == '\n')
         {
             ++line_number;
         }
@@ -144,19 +144,19 @@ void parse_number(std::string_view file_contents, size_t& i)
     size_t starting_index = i;
     double number {static_cast<double>(file_contents[i++] - '0')};
 
-    while (i < file_contents.length() && isdigit(file_contents[i]))
+    while (i < file_contents.length() && isdigit(file_contents.at(i)))
     {
         number *= 10;
         number += static_cast<double>(file_contents[i++] - '0');
     }
 
-    if (file_contents[i] == '.')
+    if (i < file_contents.length() && file_contents.at(i) == '.')
     {
         double decimal_place {0.1};
 
         while (i + 1 < file_contents.length() && isdigit(file_contents[++i]))
         {
-            number += (file_contents[i] - '0') * decimal_place;
+            number += (file_contents.at(i) - '0') * decimal_place;
             decimal_place *= 0.1;
         }
     }
